90513d56c66629a45606c6b038ca1c119edda4c4
Jacob Davies <jacobdavies@Jacobs-MacBook-Pro.local>
2022-10-20 09:25:27 -0600

    Fully Finished App

--- script.js
+++ script.js
@@ -30,6 +30,14 @@ async function renderCourse(id) {
   let ThePar = document.getElementById("par")
   let Handicap = document.getElementById("handicap")
   let teeBoxSelectHtml = ''
+  TheHoles.innerHTML = "";
+  TheYardage.innerHTML = "";
+  ThePar.innerHTML = "";
+  Handicap.innerHTML = "";
+  TheHoles.innerHTML += `<th id="holeTh">Holes</th>` ;
+  TheYardage.innerHTML += `<th id="yardTh">Yards</th>` ;
+  ThePar.innerHTML += `<th id="parTh">Par</th>`;
+  Handicap.innerHTML += `<th id="handicapTh">Handicap</th>`;
   let count = 0;
   let totalYardsArray = [];
   let totalParArray = [];
@@ -43,11 +51,11 @@ async function renderCourse(id) {
 
  })
   teeBoxes.forEach(function (teeBox, index) {
+ 
   teeBoxSelectHtml += `<option value="${index}"> PRO ${teeBox.yards} yards </option>`;
   teeBoxSelectHtml += `<option value="${index}"> CHAMPION ${teeBox.yards} yards </option>`;
   teeBoxSelectHtml += `<option value="${index}"> MEN ${teeBox.yards} yards </option>`;
   teeBoxSelectHtml += `<option value="${index}"> WOMEN ${teeBox.yards} yards </option>`;
-console.log(teeBox.yards)
   TheYardage.innerHTML += `<td>${teeBox.yards}</td>`
   ThePar.innerHTML += `<td>${teeBox.par}</td>`
   Handicap.innerHTML += `<td>${teeBox.hcp}</td>`
@@ -67,6 +75,54 @@ console.log(teeBox.yards)
 
   document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
   TheHoles.innerHTML += `<th id="totals">TOTAL</th>`
+  function nines(){
+    let out = document.createElement('th');
+    out.setAttribute('id', 'out')
+    out.innerHTML = 'OUT'
+    TheHoles.insertBefore(out, TheHoles.childNodes[10])
+    //--------------------------------------------------
+    let In = document.createElement('th');
+    In.setAttribute('id', 'In');
+    In.innerHTML = 'IN';
+    TheHoles.insertBefore(In, TheHoles.childNodes[20])
+    //--------------------------------------------------
+    let outYardage = document.createElement('th');
+    outYardage.setAttribute('id', 'outYardage');
+    let yardDummyArray = totalYardsArray.slice(0, 9)
+    console.log(yardDummyArray)
+    let outYardageTotal = yardDummyArray.reduce(TotalUp)
+    outYardage.innerHTML = outYardageTotal;
+    TheYardage.insertBefore(outYardage, TheYardage.childNodes[10])
+    //---------------------------------------------------
+    let inYardage = document.createElement('th');
+    inYardage.setAttribute('id', 'inYardage');
+    let yardInArray = totalYardsArray.slice(9);
+    let inYardageTotal = yardInArray.reduce(TotalUp);
+    inYardage.innerHTML = inYardageTotal;
+    TheYardage.insertBefore(inYardage, TheYardage.childNodes[20])
+    //---------------------------------------------------
+    let outPar = document.createElement('th');
+    outPar.setAttribute('id', 'outPar');
+    let parDummyArray = totalParArray.slice(0, 9);
+    let outParTotal = parDummyArray.reduce(TotalUp);
+    outPar.innerHTML = outParTotal;
+    ThePar.insertBefore(outPar, ThePar.childNodes[10])
+    //---------------------------------------------------
+    let inPar = document.createElement('th');
+    inPar.setAttribute('id', 'inPar');
+    let inParArray = totalParArray.slice(9);
+    let inParTotal = inParArray.reduce(TotalUp);
+    inPar.innerHTML = inParTotal;
+    ThePar.insertBefore(inPar, ThePar.childNodes[20])
+    //---------------------------------------------------
+    let outHandicap = document.createElement('th');
+    outHandicap.setAttribute('id', 'outHandicap')
+    Handicap.insertBefore(outHandicap, Handicap.childNodes[10])
+    //---------------------------------------------------
+    Handicap.innerHTMl += `<th id="inHandicap"></th>`
+    Handicap.innerHTML += `<th id="totalHandicap"></th>`
+  }
+  nines();
 }
 
 function updateCourseSelected() {
@@ -171,10 +227,18 @@ let p4ArrayNum = [];
 function postScore4(){
   let newLocation = document.getElementById('emptyDiv')
   let score = document.getElementById('scores')
-  document.getElementById('player4row').innerHTML += `<td>${scores.value}</td>`
-  p4Array.push(scores.value)
+  document.getElementById('player4row').innerHTML += `<td>${score.value}</td>`
+  p4Array.push(Number(score.value))
   let p4row = document.getElementById('player4row')
-  if(p4row.childElementCount == 19) {
+  if(p4row.childElementCount == 10){
+    let playerOut = document.createElement('th');
+    playerOut.setAttribute('id', 'playerOut');
+    let playerOutArray = p4Array.slice(0, 9);
+    let pOutArrayTotal = playerOutArray.reduce(TotalUp);
+    playerOut.innerHTML = pOutArrayTotal;
+    p4row.insertBefore(playerOut, p4row.childNodes[10])
+  }
+  else if(p4row.childElementCount == 20) {
     for(let i = 0; i < p4Array.length; i++){
       p4ArrayNum.push(Number(p4Array[i]))
     }
@@ -188,7 +252,13 @@ function postScore4(){
     // <button class="btn btn-primary" onclick="addList4()">New List</button>
     //  </div>`
   document.getElementById('player4row').innerHTML += `<th id="totals">${p4Total}</th>`
-
+  //------------------------------------------------------------------------------------
+  let playerIn = document.createElement('th');
+  playerIn.setAttribute('id', 'playerIn');
+  let playerInArray = p4Array.slice(9);
+  let pInArrayTotal = playerInArray.reduce(TotalUp);
+  playerIn.innerHTML = pInArrayTotal;
+  p4row.insertBefore(playerIn, p4row.childNodes[20])
   }}
 function buttonClicked3(){
   let table = document.getElementById('tableSection')
@@ -205,10 +275,18 @@ let p3ArrayNum = [];
 function postScore3(){
   let newLocation = document.getElementById('emptyDiv')
   let score = document.getElementById('scores')
-  document.getElementById('player3row').innerHTML += `<td>${scores.value}</td>`
-  p3Array.push(scores.value)
+  document.getElementById('player3row').innerHTML += `<td>${score.value}</td>`
+  p3Array.push(Number(score.value))
   let p3row = document.getElementById('player3row')
-  if(p3row.childElementCount == 19) {
+  if(p3row.childElementCount == 10){
+    let playerOut = document.createElement('th');
+    playerOut.setAttribute('id', 'playerOut');
+    let playerOutArray = p3Array.slice(0, 9);
+    let pOutArrayTotal = playerOutArray.reduce(TotalUp);
+    playerOut.innerHTML = pOutArrayTotal;
+    p3row.insertBefore(playerOut, p3row.childNodes[10])
+  }
+  else if(p3row.childElementCount == 20) {
     for(let i = 0; i < p3Array.length; i++){
       p3ArrayNum.push(Number(p3Array[i]))
     }
@@ -222,6 +300,13 @@ function postScore3(){
     <button class="btn btn-primary" onclick="addList4()">New Player</button>
      </div>`
   document.getElementById('player3row').innerHTML += `<th id="totals">${p3Total}</th>`
+  //--------------------------------------------------------------------------------
+    let playerIn = document.createElement('th');
+    playerIn.setAttribute('id', 'playerIn');
+    let playerInArray = p3Array.slice(9);
+    let pInArrayTotal = playerInArray.reduce(TotalUp);
+    playerIn.innerHTML = pInArrayTotal;
+    p3row.insertBefore(playerIn, p3row.childNodes[20])
 
   }
 }
@@ -240,10 +325,18 @@ let p2ArrayNum = [];
 function postScore2(){
   let newLocation = document.getElementById('emptyDiv')
   let score = document.getElementById('scores')
-  document.getElementById('player2row').innerHTML += `<td>${scores.value}</td>`
-  p2Array.push(scores.value)
+  document.getElementById('player2row').innerHTML += `<td>${score.value}</td>`
+  p2Array.push(Number(score.value))
   let p2row = document.getElementById('player2row')
-  if(p2row.childElementCount == 19) {
+  if(p2row.childElementCount == 10){
+    let playerOut = document.createElement('th');
+    playerOut.setAttribute('id', 'playerOut');
+    let playerOutArray = p2Array.slice(0, 9);
+    let pOutArrayTotal = playerOutArray.reduce(TotalUp);
+    playerOut.innerHTML = pOutArrayTotal;
+    p2row.insertBefore(playerOut, p2row.childNodes[10])
+  }
+  else if(p2row.childElementCount == 20) {
     for(let i = 0; i < p2Array.length; i++){
       p2ArrayNum.push(Number(p2Array[i]))
     }
@@ -257,6 +350,13 @@ function postScore2(){
     <button class="btn btn-primary" onclick="addList3()">New Player</button>
      </div>`
   document.getElementById('player2row').innerHTML += `<th id="totals">${p2Total}</th>`
+  //-----------------------------------------------------------------------------------
+  let playerIn = document.createElement('th');
+  playerIn.setAttribute('id', 'playerIn');
+  let playerInArray = p2Array.slice(9);
+  let pInArrayTotal = playerInArray.reduce(TotalUp);
+  playerIn.innerHTML = pInArrayTotal;
+  p2row.insertBefore(playerIn, p2row.childNodes[20])
 
   }
 }
@@ -280,7 +380,15 @@ function postScore(){
   document.getElementById('player1row').innerHTML += `<td>${score.value}</td>`
   p1Array.push(Number(score.value))
   let p1row = document.getElementById('player1row')
-    if(p1row.childElementCount == 19) {
+    if(p1row.childElementCount == 10){
+      let playerOut = document.createElement('th');
+      playerOut.setAttribute('id', 'playerOut');
+      let playerOutArray = p1Array.slice(0, 9);
+      let pOutArrayTotal = playerOutArray.reduce(TotalUp);
+      playerOut.innerHTML = pOutArrayTotal;
+      p1row.insertBefore(playerOut, p1row.childNodes[10])
+    }
+    else if(p1row.childElementCount == 20) {
     for(let i = 0; i < p1Array.length; i++){
       p1ArrayNum.push(p1Array[i])
     }
@@ -294,17 +402,17 @@ function postScore(){
     <button class="btn btn-primary" onclick="addList2()">New Player</button>
      </div>`
   document.getElementById('player1row').innerHTML += `<th id="totals">${p1Total}</th>`
-
+  //---------------------------------------------------------------------------------
+      let playerIn = document.createElement('th');
+      playerIn.setAttribute('id', 'playerIn');
+      let playerInArray = p1Array.slice(9);
+      let pInArrayTotal = playerInArray.reduce(TotalUp);
+      playerIn.innerHTML = pInArrayTotal;
+      p1row.insertBefore(playerIn, p1row.childNodes[20])
   }
-  // function nines(){
-  //   let outScore = p1Array.slice(0, 9)
-  //   let scoreOut = outScore.reduce(TotalUp)
-  //   let inScore = p1Array.slice(10, 19);
-  //   console.log(p1Array)
+ 
+ 
 
-    
-  // }
-  // nines();
 }
 
   // //============================================================================
